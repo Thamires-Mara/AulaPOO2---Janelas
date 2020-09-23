ee2bc6153110fc4280469aa63b8020dd1a66be0b
Loop <thamires.mara@outlook.com.br>
2020-09-23 16:47:37 -0300

    Commit janela de Login

---
+++ src/br/com/controller/UserLoger.java
@@ -0,0 +1,17 @@
+package br.com.controller;
+
+import br.com.model.User;
+
+public class UserLoger {
+	
+	public boolean validacaoLogin(String login, String senha) {
+		
+		User user = dummyUser();
+		
+		return user.getLogin().equals (login) && user.getSenha().equals(senha);
+	}
+
+	public User dummyUser () {
+		return new User ();
+	}
+}
---
+++ src/br/com/view/LoginUser.java
@@ -0,0 +1,96 @@
+package br.com.view;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import br.com.controller.UserLoger;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class LoginUser extends JFrame {
+
+	protected static final Component ContentPane = null;
+	private JPanel contentPane;
+	private JTextField textFieldLogin;
+	private JPasswordField passwordFieldSenha;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginUser frame = new LoginUser();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginUser() {
+		setTitle("Janela de Login");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Login");
+		lblNewLabel.setBounds(50, 48, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Senha");
+		lblNewLabel_1.setBounds(50, 89, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		textFieldLogin = new JTextField();
+		textFieldLogin.setBounds(117, 45, 207, 20);
+		contentPane.add(textFieldLogin);
+		textFieldLogin.setColumns(10);
+		
+		passwordFieldSenha = new JPasswordField();
+		passwordFieldSenha.setBounds(117, 86, 207, 20);
+		contentPane.add(passwordFieldSenha);
+		
+		JButton btnNewButtonLogar = new JButton("Logar");
+		btnNewButtonLogar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				UserLoger us = new UserLoger();
+				
+			JLabel passwordFielSenha;
+			if	(us.validacaoLogin(textFieldLogin.getText(), passwordFielSenha.getText())){
+			JLabel textFildeLogin;
+			JOptionPane.showMessageDialog(ContentPane, "Ok Login!", + textFieldLogin.getText() + "Login efetuado" , JOptionPane .YES_OPTION);
+		}else {
+			JOptionPane . showMessageDialog (contentPane, " Desculpe, mas você errou o login ou a senha " , " Login negado! " , JOptionPane . ERROR_MESSAGE );
+		}
+			
+			    textFieldLogin.setText ("");
+				passwordFieldSenha.setText ("");
+				
+				
+			}
+		});
+		btnNewButtonLogar.setBounds(314, 196, 97, 41);
+		contentPane.add(btnNewButtonLogar);
+	}
+}
